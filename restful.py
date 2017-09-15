74451aacce5804bb228c1505cc8e46f8c00dce6e
Artur <github@artursterz.de>
2017-09-15 10:45:22 +0200

    Added restful docs.

--- restful.py
+++ restful.py
@@ -1,24 +1,50 @@
+'''Module for maintaining a RESTful Connection.
+'''
 import requests
-import keyring, rhizome
+import keyring
+import rhizome
 
-class RestfulConnection:
+class RestfulConnection(object):
+    '''Class for maintaining a RESTful connection to Serval.
+    Args:
+        host (str):     The IP to the Serval host (default: localhost)
+        port (int):     The port to the Serval host (default: 4110)
+        user (str):     Username for the RESTful Server
+        passwd (str):   Password fpr the RESTful Server
+    '''
     def __init__(self, host="localhost", port=4110, user="pyserval", passwd="pyserval"):
-        self._AUTH = (user, passwd)
-        self._BASE = "http://{}:{}".format(host, port)
-        
+        self._auth = (user, passwd)
+        self._base = "http://{}:{}".format(host, port)
+
         self.keyring = keyring.Keyring(self)
         self.rhizome = rhizome.Rhizome(self)
         self.first_identity = self.keyring.get_first_identity()
 
     def get(self, path, **params):
-        request = requests.get(self._BASE+path, auth=self._AUTH, **params)
+        '''GET request.
+        Args:
+            path (str):     URL path for the GET.
+            params (dict):  Dictionary with parameters for the Request.
+
+        Returns:
+            The Response object.
+        '''
+        request = requests.get(self._base+path, auth=self._auth, **params)
         request.raise_for_status()
         return request
-    
+
     def post(self, path, **params):
-        request = requests.post(self._BASE+path, auth=self._AUTH, **params)
+        '''POST request.
+        Args:
+            path (str):     URL path for the POST.
+            params (dict):  Dictionary with parameters for the Request.
+
+        Returns:
+            The Response object.
+        '''
+        request = requests.post(self._base+path, auth=self._auth, **params)
         request.raise_for_status()
         return request
-        
+
     def __repr__(self):
-        return "RestfulConnection(\"{}, {}\")".format(self._BASE, self._AUTH)
+        return "RestfulConnection(\"{}, {}\")".format(self._base, self._auth)
